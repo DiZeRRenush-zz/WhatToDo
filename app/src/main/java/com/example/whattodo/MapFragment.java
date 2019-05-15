5d6e12ee32b6b6712681bbca55e069e705f6df72
Dmitriy Zhernov <dima555zhernov@gmail.com>
2019-05-15 14:17:24 +0500

    Add bottom menu

---
+++ app/src/main/java/com/example/whattodo/MapFragment.java
@@ -0,0 +1,22 @@
+package com.example.whattodo;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MapFragment extends Fragment {
+
+    public MapFragment() {
+    }
+
+    public static MapFragment newInstance() {
+        return new MapFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_map, container, false);
+    }
+}
